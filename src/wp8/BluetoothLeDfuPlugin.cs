a9f04bd23d0e46573ca8aa610ed43ede9fe669cb
Mihail Konovalov <vbitex@gmail.com>
2014-09-09 00:07:28 +0400

    wp8.1 support init

---
+++ src/wp8/BluetoothLeDfuPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+    class BluetoothLeDfuPlugin : BaseCommand
+    {
+    }
+}
